b25d813a7fdeecbdf0a4892a245a432632ff49d5
shivamshah1205 <shivamshah1205@gmail.com>
2021-06-07 15:11:07 +0530

    Get and delete mapping

---
+++ Interview-Mini-Project/src/main/java/com/WeekendDrive/Interview/Mini/Project/Bean/Round.java
@@ -0,0 +1,63 @@
+package com.WeekendDrive.Interview.Mini.Project.Bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Round {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+
+	private int sequence;
+
+	protected Round() {}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Round [id=" + id + ", name=" + name + ", sequence=" + sequence + "]";
+	}
+
+
+
+	public Round(int id, String name, int sequence) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sequence = sequence;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+
+
+}
---
+++ Interview-Mini-Project/src/main/java/com/WeekendDrive/Interview/Mini/Project/Bean/RoundRepository.java
@@ -0,0 +1,7 @@
+package com.WeekendDrive.Interview.Mini.Project.Bean;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoundRepository extends JpaRepository<Round, Integer> {
+
+}
