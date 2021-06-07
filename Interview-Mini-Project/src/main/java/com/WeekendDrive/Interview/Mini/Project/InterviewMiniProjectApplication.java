f264085bee4b55a3dcbcecd1b6815fe65207e0c3
shivamshah1205 <shivamshah1205@gmail.com>
2021-06-07 13:48:32 +0530

    first commit

---
+++ Interview-Mini-Project/src/main/java/com/WeekendDrive/Interview/Mini/Project/InterviewMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.WeekendDrive.Interview.Mini.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMiniProjectApplication.class, args);
+	}
+
+}
---
+++ Interview-Mini-Project/src/test/java/com/WeekendDrive/Interview/Mini/Project/InterviewMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeekendDrive.Interview.Mini.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
